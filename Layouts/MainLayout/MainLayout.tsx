60043c675f4e72bdb649157c52909ea3c1a788e5
AlexAltrex <alexaltrex@mail.ru>
2022-09-06 00:29:17 +0300

    add balls

--- Layouts/MainLayout/MainLayout.tsx
+++ Layouts/MainLayout/MainLayout.tsx
@@ -7,6 +7,8 @@ import {Footer} from "../../Components/A2_Footer/Footer";
 import {ConnectWallet} from "../../Components/A3_ConnectWallet/ConnectWallet";
 import {OpenLevel} from "../../Components/A4_OpenLevel/OpenLevel";
 import {SentMessageModal} from "../../Components/A5_SentMessageModal/SentMessageModal";
+import {useRouter} from "next/router";
+import React from "react";
 
 interface IMainLayout {
     children: ReactNode
@@ -17,6 +19,9 @@ export const MainLayout: FC<IMainLayout> = ({
                                                 children,
                                                 headTitle = 'Crypto Game',
 }) => {
+    const router = useRouter();
+    //console.log(router)
+
     return (
         <div className={style.mainLayout}>
             <Head>
@@ -35,11 +40,46 @@ export const MainLayout: FC<IMainLayout> = ({
             <Header/>
             <BurgerMenu/>
 
-            <main>
+            <main className={style.main}>
                 {children}
             </main>
 
             <Footer/>
+
+            {
+                //router.pathname === "/"
+                true
+                &&
+                <div className={style.BallsFromMainPage}>
+                    {
+                        [0, 1000, 2000, 3000, 4000, 5000, 6000].map((top, index) => (
+                            <React.Fragment key={index}>
+                                <img src="/png/balls/main page/0.png"
+                                     alt=""
+                                     //className={style.ball0}
+                                     style={{
+                                         position: "absolute",
+                                         top: `${-200 + top}px`,
+                                         left: 0
+                                     }}
+                                />
+                                <img src="/png/balls/main page/1.png"
+                                     alt=""
+                                     //className={style.ball1}
+                                     style={{
+                                         position: "absolute",
+                                         top: `${200 + top}px`,
+                                         right: 0
+                                     }}
+                                />
+                            </React.Fragment>
+                        ))
+                    }
+
+                </div>
+            }
+
+
         </div>
     )
 }
