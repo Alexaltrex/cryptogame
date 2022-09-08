efe29abf3e0b08e06013c13831c14c3e992c8f71
AlexAltrex <alexaltrex@mail.ru>
2022-09-08 23:44:20 +0300

    fix

--- Components/E1_Levels/Levels.tsx
+++ Components/E1_Levels/Levels.tsx
@@ -2,23 +2,38 @@ import style from "./Levels.module.scss"
 import {H3} from "../X_Common/H3/H3";
 import {levelsData} from "./levelsData";
 import {LevelCard} from "./LevelCard/LevelCard";
+import {useState} from "react";
+import clsx from "clsx";
 
 export const Levels = () => {
+    const [showMore, setShowMore]  = useState(false);
+
     return (
         <div className={style.levels}>
             <div className={style.inner}>
 
                 <H3>CryptoGame Levels</H3>
 
-                <div className={style.cards}>
+                <div className={clsx({
+                    [style.cards]: true,
+                    [style.cards_showMore]: showMore,
+                })}>
                     <div className={style.cards_mobile}>
                         {
-                            levelsData
-                                .slice(0,7)
+                            (
+                                showMore ? levelsData : levelsData.slice(0,6)
+                            )
                                 .map((card, index) => (
                                     <LevelCard key={index} {...card}/>
                                 ))
                         }
+                        {
+                            !showMore &&
+                            <div className={style.cardWrapper}>
+                                <LevelCard  {...levelsData[6]}/>
+                            </div>
+                        }
+
                     </div>
                     <div className={style.cards_desktop}>
                         {
@@ -31,6 +46,15 @@ export const Levels = () => {
                     </div>
 
                 </div>
+
+                {
+                    !showMore &&
+                    <button className={style.moreBtn}
+                         onClick={() => setShowMore(true)}
+                >
+                    Load more
+                </button>
+                }
             </div>
         </div>
     )
